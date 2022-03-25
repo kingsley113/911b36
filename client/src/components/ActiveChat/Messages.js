4c613a2904f9949f7d1ecb629f31b5e68862cf3a
Cameron Kingsley <cameron.kingsley13@gmail.com>
2022-03-24 23:29:59 -0700

    Revert messages.js back to previous condition

--- client/src/components/ActiveChat/Messages.js
+++ client/src/components/ActiveChat/Messages.js
@@ -6,12 +6,9 @@ import moment from 'moment';
 const Messages = (props) => {
   const { messages, otherUser, userId } = props;
 
-  //sort messages by id from low to high to put in correct order
-  const sortedMessages = messages.sort((a, b) => a.id - b.id);
-
   return (
     <Box>
-      {sortedMessages.map((message) => {
+      {messages.map((message) => {
         const time = moment(message.createdAt).format('h:mm');
 
         return message.senderId === userId ? (
