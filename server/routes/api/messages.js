794ae182cfe2b3ea38529086f49e801e6cf11876
Cameron Kingsley <cameron.kingsley13@gmail.com>
2022-03-26 20:49:41 -0700

    add convo verification, improve update efficiency

--- server/routes/api/messages.js
+++ server/routes/api/messages.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const { Conversation, Message } = require("../../db/models");
 const onlineUsers = require("../../onlineUsers");
+const { Op } = require("sequelize");
 
 // expects {recipientId, text, conversationId } in body (conversationId will be null if no conversation exists yet)
 router.post("/", async (req, res, next) => {
@@ -45,22 +46,32 @@ router.post("/", async (req, res, next) => {
 
 router.patch("/read-status", async (req, res, next) => {
   try {
-    if (!req.user) {
-      return res.sendStatus(401);
-    }
-    const { messages } = req.body;
-    messages.forEach(async (message) => {
-      await Message.update(
-        { read: true },
-        {
-          where: {
-            id: message.id,
-          },
-        }
-      );
+    const { conversationId } = req.body;
+    const convoUsers = await Conversation.findOne({
+      raw: true,
+      where: {
+        id: conversationId,
+      },
+      attributes: ["user1Id", "user2Id"],
     });
 
-    res.json({ messages });
+    // Verify user is logged in and part of current conversation
+    if (!req.user || !Object.values(convoUsers).includes(req.user.id)) {
+      return res.sendStatus(401);
+    }
+
+    await Message.update(
+      { read: true },
+      {
+        where: {
+          conversationId: conversationId,
+          read: false,
+          [Op.not]: { senderId: req.user.id },
+        },
+      }
+    );
+
+    res.sendStatus(200);
   } catch (error) {
     next(error);
   }
